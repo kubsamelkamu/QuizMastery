6e9cdfcb50984835919b105f22c3b2c3aeea08ad
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-16 08:09:23 +0300

    Header title and Github icon added

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,13 +7,16 @@ function Header(){
 
     return(
         <header className={`p-4 flex justify-between items-center ${theme === 'light' ? 'bg-white text-black' : 'bg-gray-800 text-white'}`}>
-
+            
+            <div className="flex items-center space-x-2">
+                <FaGithub size={28} />
+                <h1 className="text-2xl font-bold">GitHub User Search</h1>
+            </div>
             <button 
                 onClick={toggleTheme}
                 className={`px-4 py-2 rounded ${theme === 'light' ? 'bg-gray-800 text-white' : 'bg-white text-black'}`}
             >
                 {theme === 'light' ? 'Dark Mode' : 'Light Mode'}
-
             </button>
         </header>
     );
