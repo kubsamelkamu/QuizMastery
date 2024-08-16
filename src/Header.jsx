35233d29be14cb3ed7812b88da590dcc08e7fc6b
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-16 07:55:05 +0300

    add button

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,6 +7,13 @@ function Header(){
     return(
         <header className={`p-4 flex justify-between items-center ${theme === 'light' ? 'bg-white text-black' : 'bg-gray-800 text-white'}`}>
 
+            <button 
+                onClick={toggleTheme}
+                className={`px-4 py-2 rounded ${theme === 'light' ? 'bg-gray-800 text-white' : 'bg-white text-black'}`}
+            >
+                {theme === 'light' ? 'Dark Mode' : 'Light Mode'}
+                
+            </button>
         </header>
     )
 }
