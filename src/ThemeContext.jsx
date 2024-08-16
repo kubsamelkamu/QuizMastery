7025cddd8908ff9813dff0b8d2922175a364ad1f
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-16 07:34:59 +0300

    add state to component

--- src/ThemeContext.jsx
+++ src/ThemeContext.jsx
@@ -1,3 +1,7 @@
 import { createContext,useState } from "react";
 
 export const ThemeContext = createContext();
+
+export function ThemeProvider(){
+    const[theme,setTheme] = useState('light');
+}
