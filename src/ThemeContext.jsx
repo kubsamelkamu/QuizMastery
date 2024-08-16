d1b68f4f3bbfd932bb6c535ac043c2e6a260816c
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-16 07:37:57 +0300

    add prop

--- src/ThemeContext.jsx
+++ src/ThemeContext.jsx
@@ -2,7 +2,7 @@ import { createContext,useState } from "react";
 
 export const ThemeContext = createContext();
 
-export function ThemeProvider(){
+export function ThemeProvider({children}){
     const[theme,setTheme] = useState('light');
 
     const toggleTheme =()=>{
