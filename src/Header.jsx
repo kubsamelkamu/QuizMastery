e8999248959d62b3c29e9d56cbaf26c6575c598c
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-16 07:52:06 +0300

    add header classes

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,4 +3,10 @@ import { ThemeContext } from "./ThemeContext";
 
 function Header(){
     const{theme,toggleTheme} = useContext(ThemeContext);
+
+    return(
+        <header className={`p-4 flex justify-between items-center ${theme === 'light' ? 'bg-white text-black' : 'bg-gray-800 text-white'}`}>
+
+        </header>
+    )
 }
