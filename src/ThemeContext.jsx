ba44c3de1a45598dd01ed72772b1395c2b8a6fb2
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-16 07:40:01 +0300

    wrap in provider

--- src/ThemeContext.jsx
+++ src/ThemeContext.jsx
@@ -8,4 +8,10 @@ export function ThemeProvider({children}){
     const toggleTheme =()=>{
         setTheme((prevTheme)=>(prevTheme==='light'?'dark':'light'));
     }
+
+    return(
+        <ThemeContext.Provider value={{theme,toggleTheme}}>
+            
+        </ThemeContext.Provider>
+    )
 }
