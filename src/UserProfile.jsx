167f15829e418a3fd1008e1fb0340db9b6573051
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-16 17:08:20 +0300

    condition for user

--- src/UserProfile.jsx
+++ src/UserProfile.jsx
@@ -2,4 +2,8 @@ import { ThemeContext } from "./ThemeContext";
 import { useContext } from "react";
 function Profile({user}){
     const{theme} =useContext(ThemeContext)
+
+    if (!user) {
+        return null;
+    }
 }
