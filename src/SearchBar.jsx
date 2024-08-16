5e5d3263f0b3a701d3cc48b1a0a09ed8a5c8ea99
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-16 15:32:03 +0300

    handleInputChange method

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -9,4 +9,9 @@ function Search({onSearch}){
     const[showSuggestions,setShowSuggestions] = useState(false);
     const[activesuggestionIndex,setActivesuggestionIndex] = useState(-1);
 
+    const handleInputChange=(e)=>{
+        setInputValue(e.target.value);
+        showSuggestions(-1); // reset active suggestion
+    }
+
 }
