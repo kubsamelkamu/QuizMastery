d2c8ccac0b789b249085a066430d060e7f5bbca2
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-17 15:12:45 +0300

    theme object created

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,3 +1,7 @@
 import { useContext } from 'react';
 import { ThemeContext } from './ThemeContext';
 import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+
+function Footer(){
+    const{theme} = useContext(ThemeContext);
+}
