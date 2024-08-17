ef422e711fced1b3e01ea119d186defe875debb4
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-17 15:14:31 +0300

    add class name

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,4 +4,10 @@ import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
 function Footer(){
     const{theme} = useContext(ThemeContext);
+
+    return(
+        <footer className={`p-4 ${theme === 'light' ? 'bg-gray-200 text-gray-800' : 'bg-gray-800 text-gray-200'}`}>
+
+        </footer>
+    )
 }
