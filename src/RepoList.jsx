8f14b3d1b39ed5da55c710b309c6d1f3654922dc
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-17 08:43:00 +0300

    condition to check repo exist

--- src/RepoList.jsx
+++ src/RepoList.jsx
@@ -2,4 +2,8 @@ import { ThemeContext } from "./ThemeContext";
 import { useContext } from "react";
 function RepositoryList({repos}) {
     const theme = useContext(ThemeContext);
+
+    if (!repos || repos.length === 0)  {
+        return <p className={`text-center ${theme === 'light' ? 'text-gray-700' : 'text-gray-300'}`}>No repositories found.</p>;
+    }
 }
