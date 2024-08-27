51fa9fb370a7a4510de189f49798a05fe2f69ef6
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-27 16:28:20 +0300

    created Home component with good style

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import CategoriesSelection from '../components/categoriesSelection';
+import DifficultySelection from '../components/DifficulttSelection';
+import { useNavigate } from 'react-router-dom';
+
+function Home() {
+    const [category, setCategory] = useState('');
+    const [difficulty, setDifficulty] = useState('');
+    const navigate = useNavigate();
+
+    const handleStartQuiz = () => {
+        if (category && difficulty) {
+            navigate('/quiz', { state: { category, difficulty } });
+        } else {
+            alert('Please select both category and difficulty!');
+        }
+    };
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-400 to-blue-600 text-white">
+            <div className="text-center mb-12">
+                <h1 className="text-4xl font-extrabold mb-4">Welcome to QuizMastery!</h1>
+                <p className="text-lg mb-6">Select your preferred category and difficulty level, then start the quiz to test your knowledge!</p>
+            </div>
+            <div className=" max-w-md bg-white text-black p-8 rounded-lg shadow-lg">
+                <h2 className="text-2xl font-bold mb-6 text-center">Get Started</h2>
+                <CategoriesSelection onCategorySelect={setCategory} />
+                <DifficultySelection onDifficultySelect={setDifficulty} />
+                <button
+                    onClick={handleStartQuiz}
+                    className="mt-6 w-full px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition duration-300"
+                >
+                    Start Quiz
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
