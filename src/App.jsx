7b6566e694671e440f107a2505c1f880fdd064c7
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-27 16:01:19 +0300

    wraps components in Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-
+import { BrowserRouter as Router} from 'react-router-dom';
 import Header from './components/Header';
-
 import Footer from './components/Footer';
 import { ThemeProvider } from './context/ThemeContext';
+//import Home from './pages/Home';
+//import Quiz from './pages/Quiz';
+//import Result from './pages/Result';
 
 function App() {
-  
-
-  
     return (
         <ThemeProvider>
-            <div className="min-h-screen flex flex-col">
-                <Header />
-                <main className="flex-grow container mx-auto p-4">
-                    <div className="container mx-auto p-4">
-                      
+            <Router>
+                <div className="min-h-screen flex flex-col">
+                    <Header />
+                    <main className="flex-grow container mx-auto p-4">
                         
-                       
-                    </div>
-                </main>
-                <Footer />
-            </div>
+                    </main>
+                    <Footer />
+                </div>
+            </Router>
         </ThemeProvider>
     );
 }
