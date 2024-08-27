a7c580b5fa963988e62afffae850d4dbe5a7b091
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-27 14:10:53 +0300

    import ThemeContext

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { ThemeContext } from "./context/ThemeContext";
+import { ThemeContext } from "../context/ThemeContext";
 import {  FaSun, FaMoon } from 'react-icons/fa'; 
 
 function Header() {
