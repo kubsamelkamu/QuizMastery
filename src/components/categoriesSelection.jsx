b0434212a3c11fc71f0d0b2dd64ff7138aa501b3
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-27 15:12:14 +0300

    fetchcategories methods

--- src/components/categoriesSelection.jsx
+++ src/components/categoriesSelection.jsx
@@ -5,4 +5,17 @@ import axios  from "axios";
 function CategoriesSelection({onCategorySelection}) {
     const[categories,setCategories] = useState([]);
     const[selectedCategories,setSelectedCategories] = useState('');
+
+    const fetchcategories = async()=>{
+        try {
+            const response = await axios.get('https://opentdb.com/api_category.php');
+            setCategories(response.data.trivia_categories || []);
+        } catch (error) {
+            console.error('Error fetching categories:', error);
+        }
+    }
+
+    useEffect(()=>{
+        fetchcategories();
+    },[]);
 }
