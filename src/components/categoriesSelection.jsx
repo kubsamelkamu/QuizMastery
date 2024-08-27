6dfd49432a9e14e24f74e4410df5ade86c25a56a
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-27 15:09:14 +0300

    created component with its state

--- src/components/categoriesSelection.jsx
+++ src/components/categoriesSelection.jsx
@@ -1,3 +1,8 @@
 import { useState ,useEffect } from "react";
 import PropTypes from 'prop-types';
 import axios  from "axios";
+
+function CategoriesSelection({onCategorySelection}) {
+    const[categories,setCategories] = useState([]);
+    const[selectedCategories,setSelectedCategories] = useState('');
+}
