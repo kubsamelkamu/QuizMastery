cba683176ff70d90840a4945fc68ee8c8a6ef56c
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-29 09:22:49 +0300

    import Home and QuizQuestion and set path of routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { ThemeProvider } from './context/ThemeContext';
-//import Home from './pages/Home';
+import Home from './Pages/Home';
+import QuizQuestion from './components/QuizQuestion';
 //import Quiz from './pages/Quiz';
 //import Result from './pages/Result';
 
@@ -13,7 +14,13 @@ function App() {
                 <div className="min-h-screen flex flex-col">
                     <Header />
                     <main className="flex-grow container mx-auto p-4">
-                        
+                    <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route
+                                path="/quiz"
+                                element={<QuizQuestion />}
+                            />
+                        </Routes>
                     </main>
                     <Footer />
                 </div>
